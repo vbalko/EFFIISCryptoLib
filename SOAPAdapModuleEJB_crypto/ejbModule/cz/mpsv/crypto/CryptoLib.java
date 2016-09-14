b7af50ceec87cb10d9803c446fb0b78fad947c74
Zbynek <zbynek.svoboda@gmail.com>
2016-09-14 17:41:24 +0200

    om zb

--- SOAPAdapModuleEJB_crypto/ejbModule/cz/mpsv/crypto/CryptoLib.java
+++ SOAPAdapModuleEJB_crypto/ejbModule/cz/mpsv/crypto/CryptoLib.java
@@ -115,7 +115,7 @@ public class CryptoLib implements Module{
 	final private String ksUserParamName = "ksUser";
 	final private String ksPwdParamName = "pwd";
 	
-// promenne parametru na kanalu 55
+// promenne parametru na kanalu 55222
     private String ksView = "";
     private String ksUser = "";
     private String ksPwd = "";	
